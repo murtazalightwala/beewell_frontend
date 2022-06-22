5e73a358bb8137b1e282fd8f01f6fc1bc31aca84
murtazalightwala <murtaza.lightwala@embibe.com>
2022-06-22 20:42:05 +0530

    -

--- App.js
+++ App.js
@@ -8,25 +8,45 @@
 
 import React from 'react';
 import {Text} from 'react-native';
-import {BottomTabBar, TestNavigatorStack}  from './navigators/nav_index.js';
+import {BottomTabBar, SignUpNavigatorStack}  from './navigators/nav_index.js';
 import {NavigationContainer} from '@react-navigation/native';
+import {AsyncStorage} from 'react-native';
 
+
+async function _getUserItems() {
+  let user = await AsyncStorage.getItem("user")
+  let token = await AsyncStorage.getItem("token")
+  console.log({user, token})
+  return {user, token};
+}
 
 class App extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-          user: null,
+        "user": null,
+        "token": null
       }
   }
 
+  async update_user_data() {
+    let {user, token} = await _getUserItems();
+    this.setState({
+      "user": user,
+      "token": token
+    })
+  }
 
 render() {
-  return   <NavigationContainer >
-  <BottomTabBar />
+  this.update_user_data()
+  return <NavigationContainer >
+    { this.state.token === null ? <SignUpNavigatorStack /> : <BottomTabBar /> }
+  
 </NavigationContainer>
 
   }
 }
 
 export default App;
+
+
